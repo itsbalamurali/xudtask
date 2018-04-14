a9302296431e5961e27543b23d7a3de458cccf46
Balamurali Pandranki <balamurali@live.com>
2018-04-14 11:49:12 +0530

    Init Express Skeleton

---
+++ app.js
@@ -0,0 +1,29 @@
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.post('/orders', function(req, res) {
+  res.json('respond with a resource');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+module.exports = app;
